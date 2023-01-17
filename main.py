2834650310b42b5d90d6f8effb1fd57cec7cef39
AgentKL <agentkl57@gmail.com>
2023-01-17 13:34:45 +0100

    maze1 and ghost work

--- main.py
+++ main.py
@@ -7,54 +7,78 @@ tempList = []
 # Find the first and the last case
 for i in range(len(maze)):
     for d in range(len(maze[i])):
+        if maze[i][d] == '0' or maze[i][d] == '1':
+            continue
         if maze[i][d] == 's':
             tempList.append((i, d))
         if maze[i][d] == 'e':
             end = (i, d)
-    print(*maze[i])
+        else :
+            maze[i][d] = '1'
+    # print(*maze[i])
 
 rightWay = []
 # Tester les directions
 def VerifCase(tempList, index):
     possible = []
+    """ if index < 10:
+        print(f'tour : {index}  | {tempList}')
+    else:
+        print(f'tour : {index} | {tempList}') """
     for i in range(len(tempList)):
         # Droite
-        print(tempList)
         if maze[tempList[i][0]][tempList[i][1]+1] == '0':
             maze[tempList[i][0]][tempList[i][1]] = True
             possible.append((tempList[i][0], tempList[i][1]+1))
         if maze[tempList[i][0]][tempList[i][1]+1] == 'e':
-            print('arrivé')
+            rightWay.append(tempList)
             return 0
         # Gauche
         if maze[tempList[i][0]][tempList[i][1]-1] == '0':
             maze[tempList[i][0]][tempList[i][1]] = True
             possible.append((tempList[i][0], tempList[i][1]-1))
         if maze[tempList[i][0]][tempList[i][1]-1] == 'e':
-            print('arrivé')
+            rightWay.append(tempList)
             return 0
         # Haut
         if maze[tempList[i][0]-1][tempList[i][1]] == '0':
             maze[tempList[i][0]][tempList[i][1]] = True
             possible.append((tempList[i][0]-1, tempList[i][1]))
         if maze[tempList[i][0]-1][tempList[i][1]] == 'e':
-            print('arrivé')
+            rightWay.append(tempList)
             return 0
         # Bas
         if maze[tempList[i][0]+1][tempList[i][1]] == '0':
             maze[tempList[i][0]][tempList[i][1]] = True
             possible.append((tempList[i][0]+1, tempList[i][1]))
         if maze[tempList[i][0]+1][tempList[i][1]] == 'e':
-            print('arrivé')
+            rightWay.append(tempList)
             return 0
-
-        if len(possible) == 1:
-            rightWay.append(possible[0])
+    if possible == []:
+        print('Le labyrinthe n\a pas de solution.')
+        return 0
+    rightWay.append(tempList)
 
     tempList = possible
 
     return VerifCase(tempList, index+1)
 
 VerifCase(tempList, 0)
+rightrightway = []
+for i in range(len(rightWay), 0, -1):
+    for d in rightWay[i-1]:
+        if d[0] == end[0]+1 and d[1] == end[1]:
+            rightrightway.append(d)
+            end = d
+        elif d[0] == end[0]-1 and d[1] == end[1]:
+            rightrightway.append(d)
+            end = d
+        elif d[1] == end[1]-1 and d[0] == end[0]:
+            rightrightway.append(d)
+            end = d
+        elif d[1] == end[1]+1 and d[0] == end[0]:
+            rightrightway.append(d)
+            end = d
 
-print(rightWay)
+rightrightway = rightrightway[::-1]
+print(rightrightway)
